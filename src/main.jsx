74cf3df7a558b6adf31cf672b41ed1cd024d77f0
Joao Aecio <joaolaytynher@gmail.com>
2024-09-01 15:43:24 -0300

    navbar e sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Navbar from './pages/navbar/components/index.jsx'
+import Sidebar from './pages/sidebar/components/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Navbar/>
+    <Sidebar/>
   </StrictMode>,
 )
